b2a1db527cfa25875ff5af9d92870b45f6109d18
neo1seok prv dev <neo1seok@gmail.com>
2020-11-19 22:16:59 +0900

    add health model

--- health/models.py
+++ health/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Header(models.Model):
+
+	sys_bp = models.IntegerField(blank=True,default=None)
+	dia_bp = models.IntegerField(blank=True,default=None)
+	pulse = models.IntegerField(blank=True,default=None)
+	weight = models.FloatField(blank=True,default=None)
+	param = models.TextField(blank=True,default=None)
+	type = models.CharField(max_length=20,default=None)
+	status = models.CharField(max_length=20, default=None)
+
+
+	updt_date = models.DateTimeField(auto_now_add=True)
+	reg_date = models.DateTimeField(auto_now=True)
+
+
+	def __str__(self):
+		return self.site
+
