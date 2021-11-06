323e2749421b1eea963ec02e4eb75d1ec5f8e2cf
neo1seok prv dev <neo1seok@gmail.com>
2021-11-07 00:46:23 +0900

    ver *1.0.4
    first commit and adopt version

--- neo_django_server/__init__.py
+++ neo_django_server/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 	
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 """
 #[ver].[majer].[miner]
 #ver: 전체 프레임의 격변이 있을때
@@ -7,6 +7,13 @@ __version__ = "1.0.3"
 #miner:버그 수정및 작은 기능 추가.
 """
 __history__ = """
+* 1.0.4
+first commit and adopt version
+ 
+ 
+
+
+	
 * 1.0.3
 first commit and adopt version
  
